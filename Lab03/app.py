95113a9ecbdbe28f969f28ee21283dce9d73ea7f
Danil <95644704+DanilKndrtv@users.noreply.github.com>
2025-10-08 01:48:53 +0500

    Update app.py

--- Lab03/app.py
+++ Lab03/app.py
@@ -7,8 +7,7 @@ load_dotenv()
 HF_TOKEN = os.getenv("HF_TOKEN")
 st.write("HF_TOKEN:", HF_TOKEN)  # проверяем, что токен подхватился
 
-API_URL = "https://api-inference.huggingface.co/models/j-hartmann/emotion-english-distilroberta-base
-"
+API_URL = "https://api-inference.huggingface.co/models/j-hartmann/emotion-english-distilroberta-base"
 headers = {"Authorization": f"Bearer {HF_TOKEN}"}
 
 def query(payload):
@@ -25,6 +24,7 @@ text = st.text_area("Введите текст:")
 if st.button("Отправить") and text:
     output = query({"inputs": text})
     st.write(output)
+
 
 
 
